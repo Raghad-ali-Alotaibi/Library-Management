ec82b8e9d25eb6a24e7281e5aee5673916a8f40c
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-04-15 17:18:19 +0300

    make a separate file for class Library

---
+++ src/LibraryItem.cs
@@ -0,0 +1,17 @@
+namespace LibraryManagement;
+
+// Base class for User and Book
+public class LibraryItem
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+
+    // Constructor
+    public LibraryItem(string name, DateTime? createdDate = null)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        CreatedDate = createdDate ?? DateTime.Now;
+    }
+}
