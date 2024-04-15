9c015be8f2c0297ba02a9b817974e79ca0d3282a
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-04-15 17:14:43 +0300

    adding DeleteUserById and DeleteBookById

--- src/Library.cs
+++ src/Library.cs
@@ -19,5 +19,48 @@ public class LibraryItem
 // class Library
 public class Library
 {
+    private List<Book> books = new List<Book>();
+    private List<User> users = new List<User>();
 
+    public void DeleteUserById(Guid id)
+    {
+        try
+        {
+            var userToDelete = users.FirstOrDefault(user => user.Id == id);
+            if (userToDelete != null)
+            {
+                users.Remove(userToDelete);
+                Console.WriteLine("User was Deleted");
+            }
+            else
+            {
+                Console.WriteLine("User not found in the Library.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The Exception: " + e.Message);
+        }
+    }
+
+    public void DeleteBookById(Guid id)
+    {
+        try
+        {
+            var bookToDelete = books.FirstOrDefault(book => book.Id == id);
+            if (bookToDelete != null)
+            {
+                books.Remove(bookToDelete);
+                Console.WriteLine("Book was Deleted");
+            }
+            else
+            {
+                Console.WriteLine("Book not found in the Library.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The Exception: " + e.Message);
+        }
+    }
 }
